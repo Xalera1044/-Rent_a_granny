e37dcc50d2c77a9e54514390c318480843e14823
Abbas Nazarov <abbas.nazarov@gmail.com>
2022-11-21 21:01:21 +0400

    updated cards

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  root to: "pages#home"
+  resources :pages
   devise_for :views
   devise_for :users
-  root to: "pages#home"
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+
 end
