ff3ae2a4cce86011010a2754545a80f5b30072a5
Christian Krawczyk <c.p.krawczyk@gmail.com>
2022-11-22 15:34:05 +0100

    grannies index is completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Creating Grannies"
+Granny.destroy_all
+user = User.create(email: "a.n@mail.com", password: "123456")
+Granny.create(first_name: "Laura", last_name: "Severus", age: "75", alive: "For Now", price_per_day: "150$", user_id: "1")
+Granny.create(first_name: "Stephanie", last_name: "Lazarius", age: "89", alive: "Nope", price_per_day: "0$", user_id: "1")
+Granny.create(first_name: "Maria", last_name: "Pouline", age: "96", alive: "Nope", price_per_day: "0$", user_id: "1")
+Granny.create(first_name: "Elizabeth", last_name: "Adisson", age: "79", alive: "For Now", price_per_day: "1500$", user_id: "1")
+Granny.create(first_name: "Sevda", last_name: "Bailey", age: "67", alive: "Yes", price_per_day: "190$", user_id: "1")
+Granny.create(first_name: "Paula", last_name: "Hardley", age: "62", alive: "Yes", price_per_day: "260$", user_id: "1")
