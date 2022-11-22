2d6d3377b66241c12eeaa049093871624cbeb35d
Murad Hasanov <muradhov23@gmail.com>
2022-11-22 19:34:29 +0400

    create profile finished

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :views
   devise_for :users
   root to: "pages#home"
+  resources :grannies, only:[:new ,:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
